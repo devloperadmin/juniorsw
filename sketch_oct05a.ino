f8fa918dc0ccd25f2d6ebc9932067cb7d5b182d5
devloperadmin <72020354+devloperadmin@users.noreply.github.com>
2020-10-05 16:09:57 +0900

    Add files via upload

---
+++ sketch_oct05a.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo servo;
+
+int servoPin = 7;
+int angle = 0;
+
+
+void setup() {
+  servo.attach(servoPin);
+  servo.write(angle);
+for(angle = 0; angle < 130; angle++){
+    servo.write(angle);
+    delay(1);
+  }
+  delay(4500);
+  for(angle = 130; angle > 0; angle--){
+    servo.write(angle);
+    delay(1);
+  }  
+}
+
+void loop() {
+ servo.write(angle); 
+}
